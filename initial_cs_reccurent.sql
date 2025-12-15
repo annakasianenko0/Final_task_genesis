8899d5f9eaea5eae1caff7c29cee715fcc145782
annakasianenko0 <72355547+annakasianenko0@users.noreply.github.com>
2025-12-15 23:09:06 +0100

    Create initial_cs_reccurent.sql

---
+++ initial_cs_reccurent.sql
@@ -0,0 +1,14 @@
+-- initial vs reccurrent
+
+SELECT
+  card_type,
+  payment_number,
+  COUNT(*) AS total_transactions,
+  COUNTIF(is_failed) AS failed_transactions,
+  ROUND(
+    COUNTIF(is_failed) / COUNT(*) * 100, 2) AS failed_rate_pct
+FROM payments.my_payments_prepared
+WHERE card_type IS NOT NULL
+GROUP BY card_type, payment_number
+HAVING COUNT(*) >= 100
+ORDER BY failed_rate_pct DESC;
